9e501583162fcaf93f900ed6cce0ba8ce74f124c
Ryo <ryoouchi0315@gmail.com>
2020-06-06 19:01:06 +0900

    Initial Commit

---
+++ WOOT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WOOT
+//
+//  Created by Ryo on 2020/06/06.
+//  Copyright © 2020 Ryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
